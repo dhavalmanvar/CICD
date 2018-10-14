cf70a24b4386336b134343053e4c2459294070b5
Dhaval Manvar <manvardhaval.r@gmail.com>
2018-10-14 14:25:55 +0530

    Adding test cases

--- src/test/java/com/enjoy/dh/CICDSample/CicdSampleApplicationTests.java
+++ src/test/java/com/enjoy/dh/CICDSample/CicdSampleApplicationTests.java
@@ -1,16 +1,26 @@
 package com.enjoy.dh.CICDSample;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CicdSampleApplicationTests {
+	
+	@Autowired
+	private SampleController sampleController;
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testSampleController() {
+		Assert.assertEquals("Sample Application v1.0", sampleController.getVersion());
 	}
 
 }
